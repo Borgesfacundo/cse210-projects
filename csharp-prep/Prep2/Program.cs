e16e7092a0af07442e8f2882a2e778186b2b891d
Borgesfacundo <bor22016@byui.edu>
2023-09-16 13:22:51 -0600

    Finishing first week assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+
+        int grade = int.Parse(gradePercentage);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90) 
+        {
+            letter = "B";
+
+        }
+        else if (grade >= 70 && grade < 80) 
+        {
+            letter = "C";
+
+        }
+        else if (grade >= 60 && grade < 70) 
+        {
+            letter = "D";
+
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+            if (grade >= 70)
+            {
+                Console.WriteLine("Congratulations, you passed the course!");
+            }
+            else 
+            {
+                Console.WriteLine("Sorry, but you didn't pass the course!");
+                Console.WriteLine("You will do it better next time");
+
+
+            }
     }
 }
