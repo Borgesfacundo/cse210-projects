099de2b2f582528b7569c61a5aa1599fd9bb1739
Borgesfacundo <bor22016@byui.edu>
2023-09-20 20:13:21 -0600

    Third Prep completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        Random RandomGenerator = new Random();
+        int mNumber = RandomGenerator.Next(1, 100);
+
+        int guess = 0;
+
+        while (guess != mNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < mNumber)
+            {
+                Console.WriteLine("Lower! ");
+            }
+            else if (guess > mNumber)
+            {
+                Console.WriteLine("higher ");
+            }
+            else 
+            {
+                Console.WriteLine("You Guessed it!");
+            }
+        }
     }
 }
