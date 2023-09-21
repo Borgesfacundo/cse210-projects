826ad93dbd6e676f9f622fa37b597c3291973751
Borgesfacundo <bor22016@byui.edu>
2023-09-20 20:28:00 -0600

    Street Challenge completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,27 +5,31 @@ class Program
     static void Main(string[] args)
     {
         // Console.Write("What is the magic number? ");
-        Random RandomGenerator = new Random();
-        int mNumber = RandomGenerator.Next(1, 100);
+        Random randomGenerator = new Random();
+        int mNumber = randomGenerator.Next(1, 100);
 
         int guess = 0;
+        int tries = 0;
 
         while (guess != mNumber)
         {
             Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
 
+            tries += 1; 
+
             if (guess < mNumber)
             {
-                Console.WriteLine("Lower! ");
+                Console.WriteLine("Higher! ");
             }
             else if (guess > mNumber)
             {
-                Console.WriteLine("higher ");
+                Console.WriteLine("Lower ");
             }
             else 
             {
                 Console.WriteLine("You Guessed it!");
+                Console.WriteLine($"You took {tries} tries! ");
             }
         }
     }
