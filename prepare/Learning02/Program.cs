268d1e4add7401f221470cba847b0cc21a13b816
Borgesfacundo <bor22016@byui.edu>
2023-09-27 20:41:18 -0600

    Week 3 Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._company = "Amazon";
+        job1._jobTitle = "Administrative Assistant";
+        job1._startYear = 2019;
+        job1._endYear = 2020;
+
+
+        Job job2 = new Job();
+
+        job2._company = "Google";
+        job2._jobTitle = "Front-End Developer";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        Resume resume = new Resume();
+        resume._name = "Michael Jackson";
+
+        resume._listOfJobs.Add(job1);
+        resume._listOfJobs.Add(job2);
+
+
+        resume.Display();
     }
 }
