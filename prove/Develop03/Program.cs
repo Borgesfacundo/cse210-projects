7c2f147938a34693716b8656806aa00f64eba09e
Borgesfacundo <bor22016@byui.edu>
2023-10-19 04:37:35 -0600

    Program is showing reference but not scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -9,9 +9,10 @@ class Program
         
         //Displays References with only one verse
         Reference Reference = new("John", "3", "16");
-
+        Reference.GetDisplayReference();
         //Create a Scripture Object
         Scripture Scripture = new(Reference, "God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        Console.WriteLine(Scripture.GetRenderedText());
 
 
         //Display references with more than one verse
